2c3983ef92a171f0b6158fd8227959d059dcadc5
Ani-E-cyber <112654437+Ani-E-cyber@users.noreply.github.com>
2024-02-15 16:05:23 +0400

    Update script.js

--- script.js
+++ script.js
@@ -12,3 +12,15 @@ function countWords() {
   const result = `Words: ${wordCount}<br>Sentences: ${sentenceCount}<br>Letters: ${letterCount}`
   document.getElementById('wordCountResult').innerHTML = result
 }
+
+
+function generateRandomBackground() {
+  const backgroundPhotos = [
+    'url("https://images.unsplash.com/photo-1565118531796-763e5082d113?q=80&w=1470&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D")',
+    'url("https://images.unsplash.com/photo-1507525428034-b723cf961d3e?q=80&w=1473&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D ") ',
+    'url("https://images.unsplash.com/photo-1500534623283-312aade485b7?q=80&w=1470&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D") ',
+  ]
+
+  const randomIndex = Math.floor(Math.random() * backgroundPhotos.length)
+  document.body.style.backgroundImage = backgroundPhotos[randomIndex]
+}
